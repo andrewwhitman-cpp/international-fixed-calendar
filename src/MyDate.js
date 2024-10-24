77850b25500e9a2934a1c85c45512d6f47492159
Andrew Whitman <andrew24.whitman@gmail.com>
2024-10-23 22:10:45 -0500

    adding weekday enum and MyDate functions

--- src/MyDate.js
+++ src/MyDate.js
@@ -13,7 +13,17 @@ const MyMonth = Object.freeze({
     11: 'Undecember',
     12: 'Deuodecember',
     13: 'Tredecember'
-});
+})
+
+const MyWeekday = Object.freeze({
+    0: 'Monday',
+    1: 'Tuesday',
+    2: 'Wednesday',
+    3: 'Thursday',
+    4: 'Friday',
+    5: 'Saturday',
+    6: 'Sunday'
+})
 
 export class MyDate {
     // Constructor
@@ -22,24 +32,35 @@ export class MyDate {
         this.month = month;
         this.year = year;
     }
-  
+    
+    // Method to get day
+    getDay() { return this.day }
+    
+    // Method to get weekday name
+    getWeekday() { return Object.values(MyWeekday)[this.day] }
+    
+    // Method to get month
+    getMonth() { return this.month }
+    
+    // Method to get month name
+    getMonthName() { return Object.values(MyMonth)[this.month] }
+    
+    // Method to get year
+    getYear() { return this.year }
+
     // Method to get date
     getDate(format) {
         if (format == 'standard') {
             return this.month + '/' + this.day + '/' + this.year
         } else if (format == 'long') {
             return this.day + 'th of ' + Object.values(MyMonth)[this.month] + ' ' + this.year
+        } else if (format == 'full') {
+            return Object.values(MyWeekday)[(this.day - 1) % 7] + ' ' + 
+                    Object.values(MyMonth)[this.month] + ' ' + 
+                    this.day + ' ' + 
+                    this.year
         } else {
             return ''
         }
     }
-
-    // Method to get day
-    getDay() { return this.day }
-
-    // Method to get month
-    getMonth() { return this.month }
-
-    // Method to get year
-    getYear() { return this.year }
 }
