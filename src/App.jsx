8ad534e4153c1821b9e363e4d2b4852bd4bd473c
Andrew Whitman <andrew24.whitman@gmail.com>
2024-11-10 16:13:21 -0600

    updating app to have navigation for home and about

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { useState } from 'react'
 import { Container, Typography } from '@mui/material';
-import MyCalendar from './MyCalendar';
+import BasicMenu from './BasicMenu'
+import About from './About'
 import DateConverter from './DateConverter'
+// import MyCalendar from './MyCalendar';
 import { MyDate } from './MyDate.js'
 
 function App() {
+	const [page, setPage] = useState('Home')
+
 	var now = new Date()
 	var start = new Date(now.getFullYear(), 0, 0)
 	var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000)
@@ -15,42 +20,52 @@ function App() {
 	const my_date = new MyDate(now.getFullYear(), now.getMonth(), now.getDate())
 
 	return (
-	<Container>
-		<Typography>
-			Today is the 
-			<Typography sx={{ fontWeight: 'bold' }}>{day}th</Typography> 
-			day of the year.<br />
-		</Typography>
+		<Container >
 
-		<br />
+			<BasicMenu f={(t) => setPage(t)} />
 
-		<Typography>
-			Standard calendar date = <Typography sx={{ fontWeight: 'bold' }}>{date}</Typography>
-		</Typography>
+			{page == 'About' && <About />}
 
-		<br />
+			{page == 'Home' && <>
 
-		<Typography>
-			My calendar date<br />
-			{/* {day} / 28 = {day/28}<br /> */}
-			{my_date.getMonth()} months and {my_date.getDay()} days
-			<Typography sx={{ fontWeight: 'bold' }}>
-				{my_date.getDate('standard')}<br />
-				{my_date.getDate('full')}<br />
-			</Typography>
-		</Typography>
+				<Typography>
+					Today is the
+					<Typography sx={{ fontWeight: 'bold' }}>{day}th</Typography>
+					day of the year.<br />
+				</Typography>
 
-		<br />
+				<br />
 
-		<hr />
+				<Typography>
+					Standard calendar date = <Typography sx={{ fontWeight: 'bold' }}>{date}</Typography>
+				</Typography>
 
-		{/* <MyCalendar month={my_date.getMonthName()} day={my_date.getDay()} weekday={my_date.getWeekday()} /> */}
-		<DateConverter />
+				<br />
 
-		<br />
+				<Typography>
+					My calendar date<br />
+					{/* {day} / 28 = {day/28}<br /> */}
+					{my_date.getMonth()} months and {my_date.getDay()} days
+					<Typography sx={{ fontWeight: 'bold' }}>
+						{my_date.getDate('standard')}<br />
+						{my_date.getDate('full')}<br />
+					</Typography>
+				</Typography>
 
-		<hr />
-	</Container>
+				<br />
+
+				<hr />
+
+				{/* <MyCalendar month={my_date.getMonthName()} day={my_date.getDay()} weekday={my_date.getWeekday()} /> */}
+
+				<DateConverter />
+
+				<br />
+
+				<hr />
+			</>}
+
+		</Container>
 	)
 }
 
